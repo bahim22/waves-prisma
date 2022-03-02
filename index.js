d149679871801190c37b042f0ad5b77685ffdb4f
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-03-02 06:59:29 -0500

    -i prisma/client, & touch schema & index files; make queries to db

---
+++ index.js
@@ -0,0 +1,20 @@
+import { PrismaClient} from '@prisma/client'
+import { join } from '@prisma/client/runtime'
+
+const prisma = new PrismaClient()
+// Fetch zero or more Posts
+// const posts = await prisma.post.findMany()
+
+async function main() {
+    await prisma.$connect()
+    const allUsers = await prisma.user.findMany()
+    console.log(allUsers)
+}
+
+main()
+    .catch((e) => {
+        throw e
+    })
+    .finally(async () => {
+        await prisma.$disconnect
+    })
